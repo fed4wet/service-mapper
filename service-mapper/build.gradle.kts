90b8c046ef643bd37109818abaeea53a06ab5d01
Oleh Sypiahin <fed4wet@gmail.com>
2023-10-28 22:53:23 +0300

    JavaVersion.VERSION_17 -> JavaVersion.VERSION_21

--- service-mapper/build.gradle.kts
+++ service-mapper/build.gradle.kts
@@ -11,7 +11,7 @@ group = "com.compress"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+	sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
@@ -31,7 +31,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
+		jvmTarget = "21"
 	}
 }
 
