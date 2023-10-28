bd39183f2b16b350c08d7aef66fc06e855a3d857
Oleh Sypiahin <fed4wet@gmail.com>
2023-10-28 22:14:39 +0300

    Initial commit

---
+++ service-mapper/src/main/kotlin/com/compress/servicemapper/ServiceMapperApplication.kt
@@ -0,0 +1,11 @@
+package com.compress.servicemapper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceMapperApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServiceMapperApplication>(*args)
+}
